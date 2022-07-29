dbe21361e2ccbdd1c4939477c1517bd6720cf87f
lazdan21 <98653168+lazdan21@users.noreply.github.com>
2022-07-29 17:28:18 +0300

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -1,7 +1,7 @@
-(function(name){
+(function(window){
   var byeSpeaker = {};
   byeSpeaker.speakWord = "Good Bye";
-  byeSpeaker.speak = function (){
+  byeSpeaker.speak = function (name){
     console.log(byeSpeaker.speakWord + " " + name);
   }
 window.byeSpeaker = byeSpeaker;
